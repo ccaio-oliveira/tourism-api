ebed672cf03a1da21351da7ebc10303ba8bfa440
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-05-08 17:15:35 -0300

    feat: config api routing

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CityHistoryController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\TouristAttractionController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::apiResource('hotels', HotelController::class);
 Route::apiResource('restaurants', RestaurantController::class);
